a2e33da9928f145747350306013210ce2b54e631
Tvarita Jain <tvarita1297@gmail.com>
2018-03-03 11:53:25 +0530

    getattr,rmdir and mkdir with fuse

--- syscall.c
+++ syscall.c
@@ -279,7 +279,7 @@ int syscall_initialise_file_info(int inode_num, int file_type){
 }
 
   
-void syscall_display_stat(int inodenum){
+struct syscall_stat syscall_display_stat(int inodenum){
 	//Read the block containing the inode information
 	struct syscall_inode Inode = ReadInode(inodenum);
 	int stat_block_num = Inode.direct[0];
@@ -287,9 +287,10 @@ void syscall_display_stat(int inodenum){
 	//Read the stat block from inode
 	union syscall_block block;
 	disk_read(stat_block_num, &block);
-
+	struct syscall_stat buf;
+	buf=block.stat_info;
 	//Stat file information structure
-	printf("\nStat for inode %d",inodenum);
+	/*printf("\nStat for inode %d",inodenum);
 	printf("\n\tst_mode   : %d",block.stat_info.st_mode);
 	printf("\n\tst_ino    : %d",block.stat_info.st_ino);
 	printf("\n\tst_dev    : %d",block.stat_info.st_dev);
@@ -303,9 +304,11 @@ void syscall_display_stat(int inodenum){
 	printf("\n\tst_atim   : %lld.%.9ld", (long long)block.stat_info.st_atim.tv_sec, block.stat_info.st_atim.tv_nsec);
 	printf("\n\tst_st_mtim: %lld.%.9ld", (long long)block.stat_info.st_mtim.tv_sec, block.stat_info.st_mtim.tv_nsec);
 	printf("\n\tst_ctim   : %lld.%.9ld", (long long)block.stat_info.st_ctim.tv_sec, block.stat_info.st_ctim.tv_nsec);
-	printf("\n");
+	printf("\n");*/
+	return buf;
 }
 
+void syscall_stat(int inodenum,union * syscall_block block)
 
 /*
 	Creates the default directories . and .. 
